caff18dabe99f56dcfab771d67363cb3aee9e5e0
Jamie Pine <ijamespine@me.com>
2026-01-25 19:47:38 -0800

    Update download links and repository URL in constants.ts to reflect the correct GitHub username

--- landing/src/lib/constants.ts
+++ landing/src/lib/constants.ts
@@ -4,13 +4,13 @@ export const LATEST_VERSION = 'v0.1.0';
 
 export const DOWNLOAD_LINKS = {
   macArm:
-    'https://github.com/USERNAME/voicebox/releases/download/v0.1.0/voicebox_aarch64-apple-darwin.dmg',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_aarch64-apple-darwin.dmg',
   macIntel:
-    'https://github.com/USERNAME/voicebox/releases/download/v0.1.0/voicebox_x86_64-apple-darwin.dmg',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-apple-darwin.dmg',
   windows:
-    'https://github.com/USERNAME/voicebox/releases/download/v0.1.0/voicebox_x86_64-pc-windows-msvc.msi',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-pc-windows-msvc.msi',
   linux:
-    'https://github.com/USERNAME/voicebox/releases/download/v0.1.0/voicebox_x86_64-unknown-linux-gnu.AppImage',
+    'https://github.com/jamiepine/voicebox/releases/download/v0.1.0/voicebox_x86_64-unknown-linux-gnu.AppImage',
 } as const;
 
-export const GITHUB_REPO = 'https://github.com/USERNAME/voicebox';
+export const GITHUB_REPO = 'https://github.com/jamiepine/voicebox';
